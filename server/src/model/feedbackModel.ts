b3907fd299d018be59eb2d180df35bcb01a77578
Arun <arun11.kb@gmail.com>
2025-05-09 16:24:58 +0530

    feat: Add feedbacks schema implemented

---
+++ server/src/model/feedbackModel.ts
@@ -0,0 +1,45 @@
+import mongoose, { Date, Schema, Types } from "mongoose"
+import { FeedBackStatus } from "../interfaces/IFeedback"
+
+
+export interface IFieldsFeedback {
+  fieldId: string;
+  value: string;
+}
+
+export interface IFeedbackDb {
+  _id: Types.ObjectId
+  requestorUserId: Types.ObjectId
+  providerUserId: Types.ObjectId
+  fromId: Types.ObjectId
+  fields: IFieldsFeedback[]
+  isAnonymous: boolean
+  status: FeedBackStatus
+  rejectedReason?: string 
+  createdAt: Date
+  updatedAt: Date
+}
+
+
+const FieldsSchema = new Schema<IFieldsFeedback>({
+  fieldId: { type: String, required: true },
+  value: { type: String, required: true },
+})
+
+const FeedbackSchema = new Schema<IFeedbackDb>({
+  requestorUserId: { type: Schema.Types.ObjectId, required: true },
+  providerUserId: { type: Schema.Types.ObjectId, required: true },
+  fromId: { type: Schema.Types.ObjectId, required: true },
+  fields: { type: [FieldsSchema], required: true },
+  isAnonymous: { type: Boolean, default: false },
+  status: {
+    type: String,
+    enum: ['pending', 'completed', 'expired', 'rejected'],
+    required: true
+  },
+  rejectedReason: { type: String }
+})
+
+const feedbackModel = mongoose.model('feedbacks', FeedbackSchema)
+
+export default feedbackModel
