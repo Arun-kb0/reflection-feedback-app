6759058404c471543a6f611b56568b85003d2b6d
Arun <arun11.kb@gmail.com>
2025-05-11 00:29:28 +0530

    feat: Add signup form

--- client/src/constants/userTypes.ts
+++ client/src/constants/userTypes.ts
@@ -24,3 +24,13 @@ export type LoginFormValues = {
   email: string
   password: string
 }
+
+export type SignupFormValues = {
+  email: string
+  password: string
+  confirmPassword: string
+  firstName: string
+  lastName: string
+  department: string
+  designation: string
+}
--- client/src/pages/user/Signup.tsx
+++ client/src/pages/user/Signup.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import SignupForm from '../../components/basic/SignupForm'
+import type { SignupFormValues } from '../../constants/userTypes'
 
 type Props = {}
 
 const Signup = (props: Props) => {
+
+  const handleSubmit = (data: SignupFormValues) => {
+    console.log('signup')
+    const { confirmPassword, ...rest } = data
+    console.log(rest)
+  }
+
   return (
-    <div>Signup</div>
+    <main className='flex justify-center items-center min-h-screen '>
+      <SignupForm
+        title='Sign up'
+        onSubmit={handleSubmit}
+      />
+
+    </main>
   )
 }
 
